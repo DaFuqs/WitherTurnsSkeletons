ef4dcf0da545fe094475acf368608d132dd42d44
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-11-30 22:01:09 +0100

    Updated to 1.18

--- src/main/java/de/dafuqs/witherturnsskeletons/WitherTurnsSkeletons.java
+++ src/main/java/de/dafuqs/witherturnsskeletons/WitherTurnsSkeletons.java
@@ -10,21 +10,15 @@ import org.apache.logging.log4j.Logger;
 public class WitherTurnsSkeletons implements ModInitializer {
 
     public static final String MOD_ID = "witherturnsskeletons";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-
-
+    private static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    
     public static void log(Level logLevel, String message) {
         LOGGER.log(logLevel, "[WitherTurnsSkeletons] " +  message);
     }
-
-    public static Identifier id(String string) {
-        return new Identifier(MOD_ID, string);
-    }
-
+    
     @Override
     public void onInitialize() {
         log(Level.INFO, "Starting up...");
-
         log(Level.INFO, "Startup Finished!");
     }
 
