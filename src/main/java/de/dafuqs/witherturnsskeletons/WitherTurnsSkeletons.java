387373bf6f35384f3f5e38cb19ad28e2b44c5cbd
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-12-25 17:42:09 +0100

    fin~

--- src/main/java/de/dafuqs/witherturnsskeletons/WitherTurnsSkeletons.java
+++ src/main/java/de/dafuqs/witherturnsskeletons/WitherTurnsSkeletons.java
@@ -1,19 +1,53 @@
 package de.dafuqs.witherturnsskeletons;
 
-import net.minecraft.core.*;
+import com.mojang.serialization.*;
 import net.minecraft.core.registries.*;
+import net.minecraft.network.*;
 import net.minecraft.resources.*;
 import net.minecraft.sounds.*;
+import net.neoforged.bus.api.*;
+import net.neoforged.fml.common.*;
+import net.neoforged.neoforge.attachment.*;
+import net.neoforged.neoforge.registries.*;
+import org.jspecify.annotations.*;
 
+import java.util.function.*;
+
+@Mod(WitherTurnsSkeletons.MOD_ID)
 public class WitherTurnsSkeletons {
-
-    public static final String MOD_ID = "witherturnsskeletons";
-    
-    public static final SoundEvent CONVERTING_SOUND = registerSoundEvent("subtitles.skeleton.converted_to_wither_skeleton");
-    
-    private static SoundEvent registerSoundEvent(String name) {
-        Identifier identifier = Identifier.fromNamespaceAndPath(MOD_ID, name);
-        return Registry.register(Registries.SOUND_EVENT, identifier, SoundEvent.createVariableRangeEvent(identifier));
-    }
-
+	
+	public static final String MOD_ID = "witherturnsskeletons";
+	
+	private static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES = DeferredRegister.create(NeoForgeRegistries.ATTACHMENT_TYPES, MOD_ID);
+	private static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(Registries.SOUND_EVENT, MOD_ID);
+	
+	public static final SoundEvent CONVERTING_SOUND = registerSoundEvent("subtitles.skeleton.converted_to_wither_skeleton");
+	
+	public static final Supplier<AttachmentType<Integer>> WITHER_SKELETON_CONVERSION_TIME = ATTACHMENT_TYPES.register("wither_skeleton_conversion_time", () -> AttachmentType.builder(() -> 0)
+			.sync(new AttachmentSyncHandler<>() {
+				@Override
+				public void write(@NonNull RegistryFriendlyByteBuf buf, Integer attachment, boolean initialSync) {
+					buf.writeInt(attachment);
+				}
+				
+				@Override
+				public @NonNull Integer read(@NonNull IAttachmentHolder holder, @NonNull RegistryFriendlyByteBuf buf, @Nullable Integer previousValue) {
+					return buf.readInt();
+				}
+			})
+			.serialize(Codec.INT.fieldOf("wither_skeleton_conversion_time"))
+			.build()
+	);
+	
+	public WitherTurnsSkeletons(IEventBus modBus) {
+		ATTACHMENT_TYPES.register(modBus);
+		SOUND_EVENTS.register(modBus);
+	}
+	
+	private static SoundEvent registerSoundEvent(String name) {
+		SoundEvent soundEvent = SoundEvent.createVariableRangeEvent(Identifier.fromNamespaceAndPath(MOD_ID, name));
+		SOUND_EVENTS.register(name, () -> soundEvent);
+		return soundEvent;
+	}
+	
 }
--- src/main/java/de/dafuqs/witherturnsskeletons/mixin/SkeletonsTurnToWitherSkeletonsMixin.java
+++ src/main/java/de/dafuqs/witherturnsskeletons/mixin/SkeletonsTurnToWitherSkeletonsMixin.java
@@ -1,38 +1,30 @@
 package de.dafuqs.witherturnsskeletons.mixin;
 
 import de.dafuqs.witherturnsskeletons.*;
-import net.minecraft.entity.*;
-import net.minecraft.entity.conversion.*;
-import net.minecraft.entity.data.*;
-import net.minecraft.entity.effect.*;
-import net.minecraft.entity.mob.*;
-import net.minecraft.network.syncher.*;
-import net.minecraft.storage.*;
 import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.monster.skeleton.*;
-import net.minecraft.world.level.storage.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.*;
 
+import java.util.*;
+
 @Mixin(Skeleton.class)
 public class SkeletonsTurnToWitherSkeletonsMixin {
-	
-	@Unique
-	private static final TrackedData<Boolean> CONVERTING_TO_WITHER_SKELETON = DataTracker.registerData(SkeletonEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
-	@Unique
-	private int witherSkeletonConversionTime;
 	
 	@Inject(method = "tick()V", at = @At("TAIL"))
 	public void tick(CallbackInfo ci) {
 		Skeleton thisEntity = (Skeleton) (Object) this;
 		
 		if (!thisEntity.level().isClientSide() && thisEntity.isAlive() && !thisEntity.isNoAi()) {
-			if (this.witherturnsskeletons$isConvertingToWitherSkeleton()) {
-				--witherSkeletonConversionTime;
-				if (witherSkeletonConversionTime < 0) {
+			Optional<Integer> conversionTime = thisEntity.getExistingData(WitherTurnsSkeletons.WITHER_SKELETON_CONVERSION_TIME);
+			if (conversionTime.isPresent()) {
+				int newConversionTime = conversionTime.get() - 1;
+				if (newConversionTime < 1) {
 					witherturnsskeletons$convertToWitherSkeleton();
+				} else {
+					thisEntity.setData(WitherTurnsSkeletons.WITHER_SKELETON_CONVERSION_TIME, newConversionTime);
 				}
 			} else {
 				MobEffectInstance statusEffectInstance = thisEntity.getEffect(MobEffects.WITHER);
@@ -41,11 +33,6 @@ public class SkeletonsTurnToWitherSkeletonsMixin {
 				}
 			}
 		}
-	}
-	
-	@Inject(method = "defineSynchedData(Lnet/minecraft/network/syncher/SynchedEntityData$Builder;)V", at = @At("TAIL"))
-	protected void initDataTracker(SynchedEntityData.Builder builder, CallbackInfo ci) {
-		builder.define(CONVERTING_TO_WITHER_SKELETON, false);
 	}
 	
 	@Inject(method = "isShaking()Z", at = @At("HEAD"), cancellable = true)
@@ -58,14 +45,13 @@ public class SkeletonsTurnToWitherSkeletonsMixin {
 	@Unique
 	public boolean witherturnsskeletons$isConvertingToWitherSkeleton() {
 		Skeleton thisEntity = (Skeleton) (Object) this;
-		return thisEntity.getEntityData().get(CONVERTING_TO_WITHER_SKELETON);
+		return thisEntity.hasData(WitherTurnsSkeletons.WITHER_SKELETON_CONVERSION_TIME);
 	}
 	
 	@Unique
 	public void witherturnsskeletons$setWitherSkeletonConversionTime(int time) {
-		this.witherSkeletonConversionTime = time;
 		Skeleton thisEntity = (Skeleton) (Object) this;
-		thisEntity.getEntityData().set(CONVERTING_TO_WITHER_SKELETON, true);
+		thisEntity.setData(WitherTurnsSkeletons.WITHER_SKELETON_CONVERSION_TIME, time);
 	}
 	
 	@Unique
@@ -74,19 +60,6 @@ public class SkeletonsTurnToWitherSkeletonsMixin {
 		thisEntity.convertTo(EntityType.WITHER_SKELETON, ConversionParams.single(thisEntity, true, true), (witherSkeleton) -> {
 			witherSkeleton.makeSound(WitherTurnsSkeletons.CONVERTING_SOUND);
 		});
-	}
-	
-	@Inject(method = "addAdditionalSaveData(Lnet/minecraft/world/level/storage/ValueOutput;)V", at = @At("HEAD"))
-	public void writeCustomDataToNbt(ValueOutput view, CallbackInfo ci) {
-		view.putInt("WitherSkeletonConversionTime", this.witherturnsskeletons$isConvertingToWitherSkeleton() ? this.witherSkeletonConversionTime : -1);
-	}
-	
-	@Inject(method = "readAdditionalSaveData(Lnet/minecraft/world/level/storage/ValueInput;)V", at = @At("HEAD"))
-	public void readCustomDataFromNbt(ValueInput view, CallbackInfo ci) {
-		int witherConversionTime = view.getIntOr("WitherSkeletonConversionTime", -1);
-		if (witherConversionTime > -1) {
-			this.witherturnsskeletons$setWitherSkeletonConversionTime(witherConversionTime);
-		}
 	}
 	
 }
